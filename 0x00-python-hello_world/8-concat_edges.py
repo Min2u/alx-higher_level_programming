4ca6a589db9e91a0eba49ef57f8f2413682a1605
Min2u <mintea561065@gmail.com>
2023-08-08 03:31:38 -0700

    Updated 0x00-python-hello_world

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming
+str = "Python is an interpreted, interactive, object-oriented programming\
         language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
+str = str[47:73] + str[116:119] + str[8:13]
 print(str)
