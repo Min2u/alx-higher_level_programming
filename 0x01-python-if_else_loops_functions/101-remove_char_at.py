d84dc24bd818ac88ea95d24f5e8059f07a88662e
Min2u <mintea561065@gmail.com>
2023-08-09 00:57:30 -0700

    update

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-# 100-print_tebahpla.py
-
-""""Print the alphabet in reverse order alternating upper- and lower-case."""
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+def remove_char_at(str, n):
+    if n < 0:
+        return str
+    else:
+        str = str[0:n] + str[n+1:]
+    return str
